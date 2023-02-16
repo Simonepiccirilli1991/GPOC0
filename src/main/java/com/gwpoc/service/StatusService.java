67794d572bb4a095b5e5b89902036db13e4a694d
Simonepiccirilli1991 <Kuugo91@gmail.com>
2023-02-16 15:03:47 +0100

    minor fix

--- src/main/java/com/gwpoc/service/StatusService.java
+++ src/main/java/com/gwpoc/service/StatusService.java
@@ -23,31 +23,31 @@ public class StatusService {
 	IwdbClient iwdb;
 	@Autowired
 	AnagraficaService anagrafica;
-	
+
 	Logger logger = LoggerFactory.getLogger(StatusService.class);
-	
+
 	public StatusResponse getStatus(String bt) {
 		logger.info("API :StatusService - getStatus -  START with raw request: {}", bt);
-		
+
 		StatusResponse response = new StatusResponse();
-		
+
 		// chiamo status iwdb, e vedo cosa torna se torna che utente e gia registrato faccio controllo incorciato cona anagrafica
 		StatusIwResponse iResp = iwdb.getstatus(bt);
-		
+
 		if(ObjectUtils.isEmpty(iResp)) {
 			logger.error("Client :StatusService - getStatus - EXCEPTION cause by status");
 			throw new AppException("TBD");
 		}
 		switch (iResp.getStatus()) {
-		// chiama alla status di wiam
+			// chiama alla status di wiam
 			case ConstEnum.UTENTE_NOFOUND :
 				response.setMsg(iResp.getMsg());
 				response.setStatus(ConstEnum.UTENTE_NOFOUND);
 				response.setAction(null);
 				logger.info("API :StatusService - getStatus - END with response: {}", response);
 				return response;
-				
-			
+
+
 			case ConstEnum.ACCOUNT_NOFOUND:
 				response.setMsg(iResp.getMsg());
 				response.setStatus(ConstEnum.ACCOUNT_NOFOUND);
@@ -55,31 +55,31 @@ public class StatusService {
 				response.setAction(setAction(bt));
 				logger.info("API :StatusService - getStatus - END with response: {}", response);
 				return response;
-				
+
 			case ConstEnum.REGISTRATION_FOUND:
 				response.setMsg(iResp.getMsg());
 				response.setStatus(ConstEnum.REGISTRATION_FOUND);
 				response.setAction(ActionEnum.CONSENT);
 				logger.info("API :StatusService - getStatus - END with response: {}", response);
 				return response;
-				
-				
-		default:
-			logger.error("Client :StatusService - getStatus - EXCEPTION cause by unknow status");
-			throw new AppException("Unknow Status");
+
+
+			default:
+				logger.error("Client :StatusService - getStatus - EXCEPTION cause by unknow status");
+				throw new AppException("Unknow Status");
 		}
-		
+
 	}
-	
+
 	private ActionEnum setAction(String bt) {
-		
+
 		AnagraficaResponse response = anagrafica.getAnagrafica(bt);
-		
+
 		if(ObjectUtils.isEmpty(response)) {
 			logger.error("Client :StatusService - getStatus - EXCEPTION cause by unable to retrive status");
 			throw new AppException("Error on retrive anagrafica");
 		}
-		
+
 		if(response.getMailCertificata())
 			return ActionEnum.REGISTERACCOUNT;
 		else
